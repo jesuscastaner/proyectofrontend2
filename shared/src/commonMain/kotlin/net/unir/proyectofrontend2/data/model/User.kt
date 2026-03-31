09a48bc8ca7a49af5584ed70a20f45268ce65e82
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2026-03-31 23:15:05 +0200

    feat: add support for User -> Agent relationship in data model

--- shared/src/commonMain/kotlin/net/unir/proyectofrontend2/data/model/User.kt
+++ shared/src/commonMain/kotlin/net/unir/proyectofrontend2/data/model/User.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class User(
     val id: Long,
+    val agentId: Long,
     val displayName: String,
     val username: String,
     val profilePic: String,
